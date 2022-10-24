ee93a6f0918f2c992b7cb7f51ad91a8258bfd4ea
regine <reginej2013@gmail.com>
2022-10-24 15:48:10 -0400

    New changes

--- jenkins.sh
+++ jenkins.sh
@@ -5,6 +5,10 @@
 
 echo "starting Jenkins installation this will take a few minutes.
 
+yum install wget -y
+yum install net-tools -y
+yum install sysstat -y
+yum install finger -y
 yum install java-11-openjdk-devel -y
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
@@ -14,7 +18,7 @@ systemctl status jenkins
 systemctl enable jenkins
 sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
 firewall-cmd --reload
-ifconfig
+ifconfiig
 
 echo "Jenkins is ready please get ip and type it on your browser"
 
